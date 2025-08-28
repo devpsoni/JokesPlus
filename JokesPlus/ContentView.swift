bd62818f21b52831fb5aefd6ec7b8cf1b16d66d7
devpsoni <iamdevsoni@gmail.com>
2025-08-28 17:50:16 +0530

    Initial Commit

---
+++ JokesPlus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JokesPlus
+//
+//  Created by Dev Soni on 28/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JokesPlus/JokesPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesPlusApp.swift
+//  JokesPlus
+//
+//  Created by Dev Soni on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct JokesPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
